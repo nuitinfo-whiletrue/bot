2bd560435227d77deb5e60a552200c5a1abfea35
Ivan Canet <ivan.canet@gmail.com>
2018-12-07 04:20:15 +0100

    Le bot demande le mode SARCASTIQUE ou MAMIE

--- src/main/kotlin/nuitinfo/whiletrue/bot/Bot.kt
+++ src/main/kotlin/nuitinfo/whiletrue/bot/Bot.kt
@@ -3,7 +3,10 @@ package nuitinfo.whiletrue.bot
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException
 import java.io.File
 
@@ -14,6 +17,10 @@ class Bot private constructor(private val token: String) : TelegramLongPollingBo
 
     val ressources = File("src/main/resources/")
     operator fun File.get(name: String) = File(this, name)
+
+    val users = HashMap<Int, Mode>()
+    enum class Mode { SARCASTIQUE, MAMIE }
+    fun getDBOf(user: Int) = if(users[user] == Mode.SARCASTIQUE) database else mamie
 
     init {
         println("Analyse des bases de données....")
@@ -26,8 +33,42 @@ class Bot private constructor(private val token: String) : TelegramLongPollingBo
     override fun onUpdateReceived(update: Update) {
         println(update.message?.date.toString() + "> " + update.message?.contact?.lastName + ": " + update.message?.text)
 
-        update.message?.text?.let {
-            database[it](this, update, database)
+        if (update.message?.text == "/start"){
+            SendMessage().apply {
+                chatId = update.message?.chatId.toString()
+                text = "Enchanté ${update.message?.from?.firstName?:""} !"
+                replyMarkup = InlineKeyboardMarkup().apply {
+                    keyboard = listOf(listOf(
+                        InlineKeyboardButton("Sarcastique").apply {
+                            callbackData = "MODE-SARC"
+                        },
+                        InlineKeyboardButton("Mamie-proof").apply {
+                            callbackData = "MODE-MAMIE"
+                        }
+                    ))
+                }
+            }.also { execute(it) }
+        }
+
+        update.callbackQuery?.let { cb ->
+            cb.from?.id?.let { usr ->
+                println("Reception d'un callback: ${cb.data}")
+                if (cb.data == "MODE-SARC")
+                    users[usr] = Mode.SARCASTIQUE
+                else
+                    users[usr] = Mode.MAMIE
+
+                SendMessage().apply {
+                    chatId = cb.message.chatId.toString()
+                    text = "Vous êtes maintenant en mode ${users[usr]}."
+                }.also { execute(it) }
+            }
+        }
+
+        update.message?.text?.let { text ->
+            update.message?.from?.id?.let { usr ->
+                getDBOf(usr)[text](this, update, database)
+            }
         }
     }
 
