5505dd191d2c8665407939332446577e8e5db993
Ivan Canet <ivan.canet@gmail.com>
2018-12-07 01:33:27 +0100

    Réponse aléatoire

--- src/main/kotlin/nuitinfo/whiletrue/bot/Bot.kt
+++ src/main/kotlin/nuitinfo/whiletrue/bot/Bot.kt
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException
 import java.io.File
+import kotlin.random.Random
 
 class Bot private constructor(private val token: String) : TelegramLongPollingBot() {
 
